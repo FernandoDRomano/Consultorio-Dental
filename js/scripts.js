7c00ab4d38374d9a6a64fb86c789b0e004f9fe02
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2020-03-24 18:05:14 -0300

    Agregando Sección de: Carousel de Imagenes y Bienvenido

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,50 @@ enlaces.forEach(elemento => {
     });
 });
 
+/*
+    PARA CONFIGURAR EL SLIDER CON SWIPER
+*/
+
+let mySwiper = new Swiper ('.swiper-container', {
+    //PARA QUE SE AUTO INICIE
+    //autoplay: {
+        //delay: 2000,
+    //},
+    //ACTIVAR LAS FLECHAS DE NAVEGACIÓN
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    //ACTIVANDO LA PAGINACION 
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    //PARA CONTROLAR CON LOS BOTONES DEL TECLADO
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+    //LOOP INFINITO
+    loop: true,
+    //EFECTO AL PASAR EL SLIDE
+    effect: 'fade',
+    //TIEMPO ENTRE CAMBIO DE IMAGEN
+    speed: 500,
+    //PARA MOFICAR EL CURSOR: PARA QUE APARESCA LA MANITO
+    grabCursor: true,
+})
+
+/*
+    PARA CONFIGURAR EL MODAL CON VENOBOX
+*/
+
+$(document).ready(function(){
+    $('.venobox').venobox({
+        spinner: 'cube-grid',
+        bgcolor: 'rgba(255,255,255,0.4)',
+        border: '5px',
+        closeColor: '#fff',
+        share: []
+    }); 
+});
