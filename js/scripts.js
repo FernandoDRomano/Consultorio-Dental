eb9dcb8180fb8f3c008afffe58394c822658afc7
Fernando Daniel Romano <fernandoreceptor2014@gmail.com>
2020-04-01 13:30:53 -0300

    Agregando y reemplazando librerias originales

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,9 @@ enlaces.forEach(elemento => {
 
 let mySwiper = new Swiper ('.swiper-container', {
     //PARA QUE SE AUTO INICIE
-    //autoplay: {
-        //delay: 2000,
-    //},
+    autoplay: {
+        delay: 2000,
+    },
     //ACTIVAR LAS FLECHAS DE NAVEGACIÓN
     navigation: {
         nextEl: '.swiper-button-next',
@@ -80,4 +80,61 @@ $(document).ready(function(){
 $('.counter').counterUp({
     delay: 10,
     time: 1200
+});
+
+/*
+    PARA CONFIGURAR pickadate.js
+*/
+
+$('.datepicker').pickadate({
+    monthsFull:["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+    monthsShort:["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sep","Oct","Nov","Dic"],
+    weekdaysFull:["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"],
+    weekdaysShort:["Dom","Lun","Mar","Mié","Jue","Vie","Sáb"],
+    today:"Hoy",
+    clear:"Borrar",
+    close:"Cerrar",
+    firstDay:1,
+    format:"dddd d !de mmmm !de yyyy",
+    formatSubmit:"dd/mm/yyyy",
+    selectYears: true,
+    selectMonths:true,
+    // Accessibility labels
+    labelMonthNext: 'Siguiente Mes',
+    labelMonthPrev: 'Anterior Mes',
+    labelMonthSelect: 'Seleccione un Mes',
+    labelYearSelect: 'Seleccione un Año',
+    //Fechas minimas y maximas permitidads
+    min: true, //Si coloco el valor en true no permitira seleccionar fechas anteriores
+    max: 30, //30 es la cantidad de días posteriores a la actual que se podra seleccionar, este valor se puede modificar
+});
+
+$('.timepicker').pickatime({
+    clear: 'Limpiar',
+    format: 'HH:i A',
+    //Para definir el intervalo en minutos
+    interval: 30,
+    //Para definir el intervalo de horas que mostrara
+    min: [9,00], //Empieza a partir de las 9hs
+    max: [13,00], //Hasta las 13hs
+
+});
+
+/*
+    PARA CONFIGURAR LA BARRA DE NAVEGACIÓN FIJA CON STICKIT
+*/
+
+$('#menuNavegacion').stickit({
+    className: 'menuFijo',
+});
+
+/*
+    PARA CONFIGURAR EL EFECTO SCROLL DE LA PAGINA WEB
+*/
+
+$('#menuPrincipal a').mPageScroll2id({
+    //Para indicarle el tamaño del menu de navegación fijo
+    offset: 50,
+    //Para darle la clase activo al enlace seleccionado
+    highlightClass: 'active'
 });
